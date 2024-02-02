6303003711f1bafc1c506c408bd137c5a156fa7e
htnha <hoang.tuan.nha@gmail.com>
2024-02-02 09:16:55 +0700

    Ham Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import person.Person;
+import person.Student;
+import person.Teacher;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person p1 = new Student("Dung", 20, "Dong Hoi", 8.0);
+        Person p2 = new Student("Vu", 21, "Quang Ninh", 7.5);
+        Person p3 = new Teacher("Nha",40,"Dong Hoi", 1000000);
+        Person[] list = {p1, p2, p3};
+        for (Person p:list){
+            p.display();
+        }
     }
 }
