a0c3665ed2a8f0744ea7630fa2f13c48f2a8fdc7
htnha <hoang.tuan.nha@gmail.com>
2024-02-02 08:50:10 +0700

    Tao lop Person

---
+++ src/person/Person.java
@@ -0,0 +1,54 @@
+package person;
+
+public class Person {
+    String name;
+    int age;
+    String address;
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void display(){
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+    public static void main(String[]args){
+        Person p1 = new Person("Dung", 20, "Dong HOi");
+        p1.display();
+    }
+}
